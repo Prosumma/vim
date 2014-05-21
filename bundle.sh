d5be32e239f27104efc63d59f010c61c1588a3e1
Gregory Higley <code@revolucent.net>
2014-05-21 19:40:32 -0400

    Added bundle.sh script to restore and/or update my most-used bundles.

---
+++ bundle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR=`dirname $0`;
+cd "$DIR";
+mkdir -p bundle;
+cd bundle;
+
+load_bundle() {
+  local BUNDLE="$1";
+  local REPO="$2";
+  if [ -d "$BUNDLE" ]; then
+    cd "$BUNDLE";
+    git pull;
+    cd ..;
+  elif [ \! -z "$REPO" ]; then
+    git clone "$REPO";
+  fi;
+}
+
+load_bundle ctrlp.vim 'https://github.com/kien/ctrlp.vim';
+load_bundle nerdtree 'https://github.com/scrooloose/nerdtree';
+load_bundle vim-sensible 'https://github.com/tpope/vim-sensible';
+load_bundle vim-rails 'https://github.com/tpope/vim-rails';
