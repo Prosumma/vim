15c69329430a82839c49b0f9a356ed3313fe501a
Gregory Higley <code@revolucent.net>
2015-03-27 21:34:55 -0400

    Added WindowSwap.

--- bundle.sh
+++ bundle.sh
@@ -26,4 +26,5 @@ load_bundle vim-coffee-script 'https://github.com/kchmck/vim-coffee-script';
 load_bundle vim-rebol 'https://github.com/prosumma/vim-rebol';
 load_bundle vim-pgsql 'https://github.com/prosumma/vim-pgsql';
 load_bundle vim-swift 'https://github.com/toyamarinyon/vim-swift';
+load_bundle vim-windowswap 'https://github.com/wesQ3/vim-windowswap';
 
